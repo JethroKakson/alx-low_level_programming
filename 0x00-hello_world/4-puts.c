28479041f9571e76b70ceabd869ec8a41bee005f
Jethro Kakson Nsimire <jethrokakson@gmail.com>
2022-10-16 01:56:06 -0700

    using puts to output msg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-int main()
+int main(void)
 {
-char msg[100] = "Programming is like building a multilingual puzzle\n";
+char msg[100] = "\"Programming is like building a multilingual puzzle\n";
 puts(msg);
-return 0;
+return (0);
 }
